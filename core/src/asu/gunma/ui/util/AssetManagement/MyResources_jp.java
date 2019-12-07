92647d93e789bd0c14062f906e524e391f9c8666
Michael Du <msdu1@asu.edu>
2019-12-06 20:42:27 -0700

    Added Localization Stuff

---
+++ core/src/asu/gunma/ui/util/AssetManagement/MyResources_jp.java
@@ -0,0 +1,76 @@
+package asu.gunma.ui.util.AssetManagement;
+
+public class MyResources_jp extends MyResources{
+    protected Object[][] getContents() {
+        return new Object[][] {
+            {"EnterStudentId", "生徒番号を記入してください。"},
+            {"VideoTutorials", "発音練習のビデオ "},
+            {"Flashcards", "フラッシュカード "},
+            {"LoadNewClass", "名簿を読み込む "},
+            {"DownloadClassData", "クラスデータを書き出す "},
+            {"Start", "スタート "},
+            {"Settings", "設定 "},
+            {"QuitGame", "ゲームをやめますか。"},
+            {"Yes", "はい "},
+            {"No", "いいえ "},
+            {"VocabularySets", "単語セット "},
+            {"SelectVocabularySets", "単語セットを選択してください。"},
+            {"Numbers", "数字 "},
+            {"ColorsShapes", "色・形"},
+            {"Countries", "国 "},
+            {"DaysMonths", "曜日・月"},
+            {"Feelings", "気持ち "},
+            {"Subjects", "教科 "},
+            {"FruitsFoods", "フルーツ・食べ物"},
+            {"Professions", "仕事 "},
+            {"Places", "建物 "},
+            {"Time", "時間 "},
+            {"AddVocabularySet", "単語セットを追加する。"},
+            {"VocabularySetName", "単語セット名を記入してください。"},
+            {"Cancel", "キャンセル "},
+            {"Done", "終了 "},
+
+            {"GameName", "ぐんまちゃんのハイキング"},
+            {"Game1", "ゲーム＃１"},
+            {"OptionsMenu", "オプション"},
+            {"SelectType", "Select Type:"},
+            {"Back", "戻る"},
+            {"Delete", "削除"},
+            {"SelectFile", "ファイルを選択"},
+            {"HomeKeyUnlocked", "Home key unlocked"},
+            {"GoogleLogin", "Google Login"},
+            {"GoogleLogout", "Google Logout"},
+            {"Listen", "Listen"},
+            {"Speak", "Speak"},
+            {"Previous", "前へ"},
+            {"Next", "次へ"},
+            {"Flip", "裏返す"},
+            {"Pause", "一時停止"},
+            {"Correct", "正解 "},
+
+            {"Asteroids", "Asteroids"},
+            {"WordScramble", "並び替え"},
+
+            {"GameTime", "時間の設定"},
+            {"1minute", "１分"},
+            {"2minutes", "2 分"},
+            {"3minutes", "3 分"},
+            {"4minutes", "4 分"},
+            {"5minutes", "5 分"},
+
+            {"Sound", "音量設定"},
+            {"BackgroundMusic", "BGM"},
+            {"SoundEffect", "効果音"},
+            {"Support", "サポート"},
+            {"About", "このアプリについて"},
+
+            {"Language", "言語設定"},
+            {"English", "英語"},
+            {"Japanese", "日本語"},
+
+
+
+            {"setLanguageMessage", "English"},
+        };
+    }
+}
